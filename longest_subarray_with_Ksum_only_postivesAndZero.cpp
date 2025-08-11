59e688c60b53ae095bb39fb6e9d591cf00d4461a
Sanand Tripathi <sarangpani2025@gmail.com>
2025-08-11 12:24:44 +0530

    Update longest_subarray_with_Ksum_only_postivesAndZero.cpp

--- longest_subarray_with_Ksum_only_postivesAndZero.cpp
+++ longest_subarray_with_Ksum_only_postivesAndZero.cpp
@@ -1,6 +1,8 @@
+//this solution wil only work when we have positives and zeros, it fails for negative numbers
+
 #include <bits/stdc++.h> 
 int getLongestSubarray(vector<int>& nums, int k){
-    // Write your code here
+//follows the two pointer apporach, where we will increment the right pointer only till sum==k, as it becomes sum>k we will increament left pointer while decreasign the value of sum
     int left=0;int right=0; int sum=nums[0]; int maxLen=0;
     int n=nums.size();
     while(right<n){
@@ -12,8 +14,13 @@ int getLongestSubarray(vector<int>& nums, int k){
         if(sum==k){
             maxLen=max(maxLen, right-left+1);
         }
+        //we will increment right first then only add as sum is already a[0] here
         right++;
         if(right<n) sum+=nums[right];
     }
     return maxLen;
 }
+
+//time complexity is O(2N) as first while loop runs for n times, second runs for n times overall- i.e. it does not runs n times every time otherwise it will be n^2, 
+// it runs for 1 times, then sometimes 2 times, sometimes zero times, so overall n times. hence it will be n+n and not n*n.
+//space complexity will be O(1) as we are not using any space.
